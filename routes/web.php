eb2fdc8d8dea7800e8c40c40ac4ae365c1246077
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-20 16:34:41 +0100

    build web routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main', ['MainController@index']);
+Route::get('/', 'MainController@index');
+;
+
+Route::get('/photos', function () {
+    return view('photos');
+    //return view('main');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
     //return view('main');
 });
