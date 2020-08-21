d9dbab5d7fab6d1b82ab0132481648b62624e402
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-21 14:58:52 +0100

    for each photos variable

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -1,25 +1,23 @@
-<?php $photos = ""; ?>
-
 @extends('main')
 
 @section('content')
 
-<h2>Photos List</h2>
+    <h2>Photos List</h2>
 
-@foreach( $photos as $photo )
+    @foreach( $photos as $photo )
 
-    <article class="photo-container" id="photo_one">    
+        <article class="photo-container" id="photo_one">    
 
-        <article class="photo-container" id="photo_two">
-                            
-            <h3>{{ $photo->photo_title }}</h3> (<span id="photo_id">{{ $photo->photo_number }}</span> )
-            
-            <img src= "{{ $photo->photo_url }}" alt="alt" title="title" />
-            
-        </article> 
-                    
-    </article>
+            <article class="photo-container" id="photo_two">
+                                
+                <h3>{{ $photo->photo_title }}</h3> (<span id="photo_id">{{ $photo->photo_number }}</span> )
+                
+                <img src= "{{ $photo->photo_url }}" alt="alt" title="title" />
+                
+            </article> 
+                        
+        </article>
 
-@endforeach
+    @endforeach
 
 @endsection
