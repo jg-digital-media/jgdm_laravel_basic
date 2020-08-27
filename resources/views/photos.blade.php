00dc40daa123b2f21b921c1f17d9101a9d248711
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-27 16:24:32 +0100

    photo items inline-block display

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -6,17 +6,22 @@
 
     
 
-        <article class="photo-container">
+    <article class="photo-container">
 
-            @foreach( $photos as $photo )
-                                    
-                    <h3>{{ $photo->photo_title }}</h3> (<span id="photo_id">{{ $photo->photo_number }}</span> )
-                    
-                    <img src= "{{ $photo->photo_url }}" alt="alt" title="title" />
+        @foreach( $photos as $photo )
+                                
+            <article class="photo-item">
+                
+                <div class="col-one">
+                    <img src= "{{ $photo->photo_url }}" alt="Photo: {{ $photo->photo_title }}" title="Photo: {{ $photo->photo_title }}" />
+                </div>
 
+
+                <div class="col-two">
+                    <h3>{{ $photo->photo_title }}</h3> <span id="photo_id">( {{ $photo->photo_number }} )</span>
                     <div>{{ $photo->photo_description }}</div>
-                    
-                    </title>
+                <div>
+            </article>
 
         @endforeach
     
