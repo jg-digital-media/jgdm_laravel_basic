7fbcace3f8d523ae24c1c8f5ca51507044df266a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-08-28 12:50:21 +0100

    responsive styling photos list route

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -2,22 +2,35 @@
 
 @section('content')
 
+
+<?php  
+
+$site_title = "Laravel Photo Album - Laravel Example";
+$page_title = "Photo List ";
+$site_keywords = "photography, laravel, boo, routes, databases, others.";
+$site_description = "description goes here";
+$page_url = "link to go here!";
+$page_img = "img/logo.jpg";
+
+?>
+
+
     <h2>Photos List</h2>
 
     
 
-    <article class="photo-container">
+    <article class="photo_container">
 
         @foreach( $photos as $photo )
                                 
-            <article class="photo-item">
+            <article class="photo_item">
                 
-                <div class="col-one">
+                <div class="col_one">
                     <img src= "{{ $photo->photo_url }}" alt="Photo: {{ $photo->photo_title }}" title="Photo: {{ $photo->photo_title }}" />
                 </div>
 
 
-                <div class="col-two">
+                <div class="col_two">
                     <h3>{{ $photo->photo_title }}</h3> <span id="photo_id">( {{ $photo->photo_number }} )</span>
                     <div>{{ $photo->photo_description }}</div>
                 <div>
