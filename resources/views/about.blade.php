1951f08c12a52e44ff88eb88b0bb29b7b0d4b1ce
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-08-28 11:58:06 +0100

    responsive about route

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,23 @@
 
 @section('content')
 
-    <h2>About</h2>
+<?php  
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora non autem alias officiis ea, nam doloremque optio voluptates, nostrum cumque a ipsam unde sit, eaque ut! Quisquam, voluptatibus nulla. blade conten</p>
+$site_title = "Laravel Photo Album - Laravel Example";
+$page_title = "About ";
+$site_keywords = "photography, laravel, boo, routes, databases, others.";
+$site_description = "description goes here";
+$page_url = "link to go here!";
+$page_img = "img/logo.jpg";
+
+?>
+
+    <article class="about_container">
+
+        <h2>About</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora non autem alias officiis ea, nam doloremque optio voluptates, nostrum cumque a ipsam unde sit, eaque ut! Quisquam, voluptatibus nulla. blade conten</p>
+
+    </article>
 
 @endsection
