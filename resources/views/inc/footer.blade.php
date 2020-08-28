fd09e2b0ec91d3031153fc5c6f0bc8924d3911d4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-08-28 12:56:30 +0100

    add link to project readme.md

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,7 +2,7 @@
             <footer class="footer">
                 
                 <ul class="links">
-                    <li><a href="{{env('app_url')}}/readme.md">Readme.md</a></li>
+                    <li><a href="https://github.com/jg-digital-media/laravel_basic/blob/master/README.md" target="blank">Readme.md</a></li>
                     <li><a href="/">Home</a></li>
                     <li><a href="/photos">Photos</a></li>
                     <li><a href="/about">About</a></li>
